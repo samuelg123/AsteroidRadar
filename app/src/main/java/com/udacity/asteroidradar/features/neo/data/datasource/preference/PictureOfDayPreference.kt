95c3f55c6e7744d6b3f1ab8e3fde4d4fdc1c78eb
Samuel Gunawan <samuelgunawan@mail.com>
2021-05-10 12:19:55 +0700

    Fix "inappropriate blocking method call" warning

--- app/src/main/java/com/udacity/asteroidradar/features/neo/data/datasource/preference/PictureOfDayPreference.kt
+++ app/src/main/java/com/udacity/asteroidradar/features/neo/data/datasource/preference/PictureOfDayPreference.kt
@@ -8,7 +8,6 @@ import androidx.datastore.preferences.core.stringPreferencesKey
 import com.squareup.moshi.Moshi
 import com.udacity.asteroidradar.appDataStore
 import com.udacity.asteroidradar.features.neo.data.entity.PictureOfDayEntity
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.*
 
 
@@ -20,15 +19,28 @@ class PictureOfDayPreference private constructor(context: Context) {
     private val moshi = Moshi.Builder().build()
     private val podAdapter = moshi.adapter(PictureOfDayEntity::class.java)
 
+    /**
+     * Move fromJson() to podFromJson() to fix "inappropriate blocking method call" warning.
+     * Please see https://stackoverflow.com/a/60136211
+     * Idk if this bug or something else, because kotlin [Flow] has already inside background thread.
+     */
+    private fun podFromJson(jsonString: String) = podAdapter.fromJson(jsonString)
+
     fun getPictureOfDay(): Flow<PictureOfDayEntity?> {
         return dataStore
             .data
-            .map { pref -> pref[POD]?.let { podAdapter.fromJson(it) } }
+            .map { pref -> pref[POD]?.let { podFromJson(it) } }
     }
 
     suspend fun setPictureOfDay(pod: PictureOfDayEntity) {
         dataStore.edit { pref ->
             pref[POD] = podAdapter.toJson(pod)
+        }
+    }
+
+    suspend fun clearPictureOfDay() {
+        dataStore.edit { pref ->
+            pref.remove(POD)
         }
     }
 
